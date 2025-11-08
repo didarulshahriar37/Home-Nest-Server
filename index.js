32dd5185809dcd0a4ffb8d9ce33f2c6e823cfc19
Didarul Shahriar <didarul13579@gmail.com>
2025-11-08 23:41:26 +0600

    updated resources

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.spgu1hn.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 app.get("/", (req, res) => {
     res.send('HomeNest server is running');
 })
+
+async function run () {
+    try{
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
 
 app.listen(port, () => {
     console.log(`HomeNest server is running on port : ${port}`)
