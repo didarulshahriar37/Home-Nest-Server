99c205745ed6593a309435a560ccf7d69a1d67ad
Didarul Shahriar <didarul13579@gmail.com>
2025-11-12 22:59:19 +0600

    added my-properties functionalities

--- index.js
+++ index.js
@@ -19,75 +19,70 @@ const client = new MongoClient(uri, {
 });
 
 app.get("/", (req, res) => {
-    res.send('HomeNest server is running');
+  res.send('HomeNest server is running');
 })
 
-async function run () {
-    try{
-        await client.connect();
+async function run() {
+  try {
+    await client.connect();
 
-        const db = client.db("homeNest_db");
-        const usersCollection = db.collection("users");
-        const agentsCollection = db.collection("agents");
-        const propertiesCollection = db.collection("properties");
+    const db = client.db("homeNest_db");
+    const usersCollection = db.collection("users");
+    const agentsCollection = db.collection("agents");
+    const propertiesCollection = db.collection("properties");
 
-        app.post("/all-properties", async(req, res) => {
-          const newProperty = req.body;
-          const result = await propertiesCollection.insertOne(newProperty);
-          res.send(result);
-        })
+    app.post("/all-properties", async (req, res) => {
+      const newProperty = req.body;
+      const result = await propertiesCollection.insertOne(newProperty);
+      res.send(result);
+    })
 
-        app.get("/all-properties", async(req, res) => {
-          const cursor = propertiesCollection.find();
-          const result = await cursor.toArray();
-          res.send(result);
-        })
+    app.get("/all-properties", async (req, res) => {
 
-        // app.get("/my-properties", async(req, res) => {
+      const query = {};
+      if (req.query.email) {
+        query["postedBy.email"] = req.query.email;
+      }
+      const cursor = propertiesCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
-        //   const query = {};
-        //   if(query.email === email){
-        //     query = 
-        //   }
+    app.get("/featured-properties", async (req, res) => {
+      const cursor = propertiesCollection.find().sort({ postedDate: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
-        //   const cursor = propertiesCollection.find();
-        // })
+    app.get("/agents", async (req, res) => {
+      const cursor = agentsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
-        app.get("/featured-properties", async(req, res) => {
-          const cursor = propertiesCollection.find().sort({postedDate: -1}).limit(6);
-          const result = await cursor.toArray();
-          res.send(result);
-        })
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const email = req.body.email;
+      const query = { email: email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        res.send({ message: 'User already Exists.' })
+      }
+      else {
+        const result = await usersCollection.insertOne(newUser);
+        res.send(result);
+      }
+    })
 
-        app.get("/agents", async(req, res) => {
-          const cursor = agentsCollection.find();
-          const result = await cursor.toArray();
-          res.send(result);
-        })
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  }
+  finally {
 
-        app.post("/users", async(req, res) => {
-          const newUser = req.body;
-          const email = req.body.email;
-          const query = {email:email};
-          const existingUser = await usersCollection.findOne(query);
-          if(existingUser){
-            res.send({message: 'User already Exists.'})
-          }
-          else{
-            const result = await usersCollection.insertOne(newUser);
-            res.send(result);
-          }
-        })
-
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }
-    finally{
-
-    }
+  }
 }
 run().catch(console.dir)
 
 app.listen(port, () => {
-    console.log(`HomeNest server is running on port : ${port}`)
+  console.log(`HomeNest server is running on port : ${port}`)
 })
